8fa975b31d844b6bdad671d168061f9df02a6490
Eric Ferguson <ferguson.eric.r@gmail.com>
2020-01-07 13:17:18 -0800

    Step 1 -- updated

--- ef-todo/src/reducers/reducer.js
+++ ef-todo/src/reducers/reducer.js
@@ -1,8 +1,21 @@
-export const initialState = {
-    item: 'Learn about reducers',
-    completed: false,
-    id: 3892987589
-}
+export const initialState = [
+
+    {
+        item: 'Learn about reducers',
+        completed: false,
+        id: 3892987589
+    },
+    {
+        item: 'Speak about reducers',
+        completed: false,
+        id: 3892987590
+    },
+    {
+        item: 'Teach about reducers',
+        completed: false,
+        id: 3892987591
+    }
+]
 
 export const todoReducer = (state) => {
     switch(action.type) {
